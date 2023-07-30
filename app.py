97a60299e7352d6d18883f3d2e4ab0eca5a47be1
DEYVI1 <deyvi-cabrera@live.com.mx>
2023-07-29 21:56:12 -0600

    updates

--- app.py
+++ app.py
@@ -1,19 +1,14 @@
 import pandas as pd
 import fastapi
-from typing import Optional
+from fastapi.responses import StreamingResponse  # Add to Top
 
 app = fastapi.FastAPI()
-df = pd.read_csv("data/data.csv")
-df = df.astype(str)  # Convert entire dataframe to string type for serialization
 
-
-@app.get("/search/")
-def search_jobs(title: Optional[str] = None, requirements: Optional[str] = None):
-    results = df
-    if title:
-        results = results[results["title"].str.contains(title, case=False)]
-    if requirements:
-        results = results[
-            results["requirements"].str.contains(requirements, case=False)
-        ]
-    return results.to_dict("records")
+@app.get("/csv")
+async def get_csv():
+    df = pd.read_csv("data/data.csv")
+    return StreamingResponse(
+        iter([df.to_csv(index=False)]),
+        media_type="text/csv",
+        headers={"Content-Disposition": f"attachment; filename=data.csv"},
+    )
